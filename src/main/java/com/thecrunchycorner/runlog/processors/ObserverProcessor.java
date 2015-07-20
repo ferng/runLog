86b561375c2042bde0a265825a69d8396931e636
Gonzalez, Fernando <FernandoG@motabilityoperations.co.uk>
2015-07-20 14:38:31 +0100

    Queue Processor now uses pos controller. Unit tests. Refactored packages

--- src/main/java/com/thecrunchycorner/runlog/processors/ObserverProcessor.java
+++ src/main/java/com/thecrunchycorner/runlog/processors/ObserverProcessor.java
@@ -1,39 +1,38 @@
-package com.thecrunchycorner.runlog.processors;
-
-import com.thecrunchycorner.runlog.msgstore.Store;
-import com.thecrunchycorner.runlog.msgstore.enums.OpStatus;
-import com.thecrunchycorner.runlog.processors.Processor;
-import com.thecrunchycorner.runlog.ringbufferaccess.Message;
-import com.thecrunchycorner.runlog.ringbufferaccess.PosController;
-import com.thecrunchycorner.runlog.ringbufferaccess.enums.MsgType;
-
-import java.util.Observable;
-import java.util.Observer;
-
-public abstract class ObserverProcessor implements Processor, Observer {
-    private Message msg = new Message(MsgType.NULL_MSG, 0);
-
-    public final Message getMsg() {
-        return msg;
-    }
-
-    public final void update(Observable o, Object arg) {
-        msg = (Message)arg;
-    }
-
-    public Message getMessage(Store store) {
-        return null;
-    }
-
-    public Message processMessage(Message msg) {
-        return null;
-    }
-
-    public OpStatus putMessage(Store store, Message msg) {
-        return null;
-    }
-
-    public boolean updatePos(PosController pCtrlr) {
-        return false;
-    }
-}
+//package com.thecrunchycorner.runlog.processors;
+//
+//import com.thecrunchycorner.runlog.msgstore.Store;
+//import com.thecrunchycorner.runlog.msgstore.enums.OpStatus;
+//import com.thecrunchycorner.runlog.ringbufferaccess.Message;
+//import com.thecrunchycorner.runlog.ringbufferaccess.PosController;
+//import com.thecrunchycorner.runlog.ringbufferaccess.enums.MsgType;
+//
+//import java.util.Observable;
+//import java.util.Observer;
+//
+//public abstract class ObserverProcessor implements Processor, Observer {
+//    private Message msg = new Message(MsgType.NULL_MSG, 0);
+//
+//    public final Message getMsg() {
+//        return msg;
+//    }
+//
+//    public final void update(Observable o, Object arg) {
+//        msg = (Message)arg;
+//    }
+//
+//    public Message getMessage(Store store) {
+//        return null;
+//    }
+//
+//    public Message processMessage(Message msg) {
+//        return null;
+//    }
+//
+//    public OpStatus putMessage(Store store, Message msg) {
+//        return null;
+//    }
+//
+//    public boolean updatePos(PosController pCtrlr) {
+//        return false;
+//    }
+//}
--- src/test/groovy/com/thecrunchycorner/runlog/processors/ProcessorGetMsgObjectSpec.groovy
+++ src/test/groovy/com/thecrunchycorner/runlog/processors/ProcessorGetMsgObjectSpec.groovy
@@ -1,57 +1,55 @@
-package com.thecrunchycorner.runlog.processors
-
-import com.thecrunchycorner.runlog.msgstore.LinkedBlockingQueueStore
-import com.thecrunchycorner.runlog.msgstore.RingBufferStore
-import com.thecrunchycorner.runlog.processors.ObserverProcessor
-import com.thecrunchycorner.runlog.ringbufferaccess.Message
-import com.thecrunchycorner.runlog.ringbufferaccess.enums.MsgType
-import com.thecrunchycorner.runlog.services.SystemProperties
-
-import spock.lang.Specification
-
-class ProcessorGetMsgObjectSpec extends Specification {
-
-    def 'test'() {
-        given:
-        //Mocks are broken for classes (what???) using the real class instead
-        def bufferSize = Integer.parseInt(SystemProperties.get("threshold.buffer.minimum.size"))
-        def qStore = new LinkedBlockingQueueStore()
-        def ringStore = new RingBufferStore(bufferSize)
-
-
-
-
-
-        def random = new Random()
-        def processor = new ObserverProcessor()
-        def notifier = new Notifier()
-        def newValue = random.nextInt()
-        def oldValue = random.nextInt()
-        def msg = new Message(MsgType.QUEUE_PAYLOAD, oldValue)
-
-        when:
-        notifier.addObserver(processor)
-        notifier.setVal(msg, newValue)
-        notifier.notifyObservers()
-
-        then:
-        processor.getMsg().getPayload() == newValue
-    }
-
-
-    public static class Notifier extends Observable {
-        def msg
-
-        void setVal(Message msg, int val) {
-            this.msg = msg
-            msg.setPayload(val)
-        }
-
-        @Override
-        void notifyObservers() {
-            setChanged()
-            notifyObservers(msg)
-        }
-    }
-
-}
+//package com.thecrunchycorner.runlog.processors
+//import com.thecrunchycorner.runlog.msgstore.LinkedBlockingQueueStore
+//import com.thecrunchycorner.runlog.msgstore.RingBufferStore
+//import com.thecrunchycorner.runlog.ringbufferaccess.Message
+//import com.thecrunchycorner.runlog.ringbufferaccess.enums.MsgType
+//import com.thecrunchycorner.runlog.services.SystemProperties
+//
+//import spock.lang.Specification
+//
+//class ProcessorGetMsgObjectSpec extends Specification {
+//
+//    def 'test'() {
+//        given:
+//        //Mocks are broken for classes (what???) using the real class instead
+//        def bufferSize = Integer.parseInt(SystemProperties.get("threshold.buffer.minimum.size"))
+//        def qStore = new LinkedBlockingQueueStore()
+//        def ringStore = new RingBufferStore(bufferSize)
+//
+//
+//
+//
+//
+//        def random = new Random()
+//        def processor = new ObserverProcessor()
+//        def notifier = new Notifier()
+//        def newValue = random.nextInt()
+//        def oldValue = random.nextInt()
+//        def msg = new Message(MsgType.QUEUE_PAYLOAD, oldValue)
+//
+//        when:
+//        notifier.addObserver(processor)
+//        notifier.setVal(msg, newValue)
+//        notifier.notifyObservers()
+//
+//        then:
+//        processor.getMsg().getPayload() == newValue
+//    }
+//
+//
+//    public static class Notifier extends Observable {
+//        def msg
+//
+//        void setVal(Message msg, int val) {
+//            this.msg = msg
+//            msg.setPayload(val)
+//        }
+//
+//        @Override
+//        void notifyObservers() {
+//            setChanged()
+//            notifyObservers(msg)
+//        }
+//    }
+//
+//}
