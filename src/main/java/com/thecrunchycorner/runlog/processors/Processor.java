27554a69d9e5c61d80c19c2340dc51d6daba1705
Gonzalez, Fernando <FernandoG@motabilityoperations.co.uk>
2015-07-20 17:09:13 +0100

    queue processor now reads correct processor pos. method refactoring. tests

--- src/main/java/com/thecrunchycorner/runlog/processors/Processor.java
+++ src/main/java/com/thecrunchycorner/runlog/processors/Processor.java
@@ -1,16 +1,15 @@
 package com.thecrunchycorner.runlog.processors;
 
-import com.thecrunchycorner.runlog.msgstore.Store;
 import com.thecrunchycorner.runlog.msgstore.enums.OpStatus;
 import com.thecrunchycorner.runlog.ringbufferaccess.Message;
-import com.thecrunchycorner.runlog.ringbufferaccess.PosController;
+import com.thecrunchycorner.runlog.ringbufferaccess.enums.ProcessorType;
 
-public interface Processor {
-    public Message getMessage(Store store);
+public abstract class Processor {
+    protected  abstract Message getMessage();
 
-    public Message processMessage(Message msg);
+    protected abstract Message processMessage(Message msg);
 
-    public OpStatus putMessage(Store store, Message msg);
+    protected abstract OpStatus putMessage(Message msg);
 
-    public boolean updatePos(PosController pCtrlr);
+    protected abstract void nextPos(ProcessorType prcType);
 }
