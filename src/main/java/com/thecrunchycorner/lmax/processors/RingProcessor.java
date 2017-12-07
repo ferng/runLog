82ca9d46099864d0500c050b0d5492ee201200db
ferng001 <ferng001@gmail.com>
2017-12-07 06:58:20 +0000

    get rid of processor hierarchy

--- src/main/java/com/thecrunchycorner/lmax/processors/RingProcessor.java
+++ src/main/java/com/thecrunchycorner/lmax/processors/RingProcessor.java
@@ -1,41 +1,66 @@
-//package com.thecrunchycorner.lmax.processors;
-//
-//import com.thecrunchycorner.lmax.msgstore.Message;
-//import com.thecrunchycorner.lmax.msgstore.OpStatus;
-//import com.thecrunchycorner.lmax.workflow.ProcessorWorkflow;
-//
-//public abstract class RingProcessor extends Processor{
-//    private volatile boolean interrupt = false;
-//
-//    @Override
-//    void updateHead() {
-//        int leadPos = ProcessorWorkflow.getLeadPos(props.getProcessorId());
-//        updatePos(leadPos);
-//    }
-//
-//
-//    public final boolean isInterrupt() {
-//        return interrupt;
-//    }
-//
-//    public final void setInterrupt(boolean interrupt) {
-//        this.interrupt = interrupt;
-//    }
-//
-//    public final void run() {
-//        while (!interrupt) {
-//            getAndProcessMsg();
-//        }
-//    }
-//
-//    protected final void getAndProcessMsg() {
-//        Message msg = processMessage(getMessage());
-//        while (putMessage(msg) == OpStatus.HEADER_REACHED) {
-//        }
-//    }
-//
-//
-//    public final void reqInterrupt() {
-//        interrupt = true;
-//    }
-//}
+package com.thecrunchycorner.lmax.processors;
+
+import com.thecrunchycorner.lmax.msgstore.Message;
+import com.thecrunchycorner.lmax.msgstore.OpStatus;
+import com.thecrunchycorner.lmax.processorproperties.ProcProperties;
+import com.thecrunchycorner.lmax.workflow.ProcessorWorkflow;
+
+public abstract class RingProcessor {
+    private ProcessorStatus status;
+    private volatile boolean interrupt = false;
+    private ProcProperties props;
+
+
+    public RingProcessor(ProcProperties procProperties) {
+        this.props = props;
+    }
+
+
+    void updatePos(int pos) {
+        props.setPos(pos);
+    }
+
+    void updateHead() {
+        int leadPos = ProcessorWorkflow.getLeadPos(props.getProcessorId());
+        updatePos(leadPos);
+    }
+
+    private void getAndProcessMsg() {
+        Message msg = processMessage(getMessage());
+        while (writeMessage(msg) == OpStatus.HEADER_REACHED) {
+        }
+    }
+
+    protected abstract Message getMessage();
+
+    abstract Message processMessage(Message msg);
+
+    abstract OpStatus writeMessage(Message msg);
+
+
+    ProcessorStatus getStatus() {
+        return status;
+    }
+
+    void setStatus(ProcessorStatus status) {
+        this.status = status;
+    }
+
+    public final boolean isInterrupt() {
+        return interrupt;
+    }
+
+    public final void setInterrupt(boolean interrupt) {
+        this.interrupt = interrupt;
+    }
+
+    public final void reqInterrupt() {
+        interrupt = true;
+    }
+
+    public final void run() {
+        while (!interrupt) {
+            getAndProcessMsg();
+        }
+    }
+}
